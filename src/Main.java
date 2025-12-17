d8a5b9c61d58d567d6d016f2540f148ca6d56730
hamsahesham <h.hamssa66@gmail.com>
2025-12-17 22:14:17 +0200

    add new update

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         Random random = new Random();
-
         String round=" ";
         while (!round.equals("n")) {
             System.out.print("""
